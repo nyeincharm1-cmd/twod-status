665d2b52a9bd96429136ea66088c06b83d63a24f
nyeincharm1-cmd <nyeincharm1@gmail.com>
2026-06-30 23:26:38 +0630

    server.js

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
-const express = require("express");
-const fs = require("fs");
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("TWOD Status API Running");
-});
-
 app.get("/status", (req, res) => {
-  const data = JSON.parse(fs.readFileSync("status.json"));
-  res.json(data);
-});
-
-app.listen(PORT, () => {
-  console.log("Server running...");
+    res.json({
+        status: "CLOSE",
+        open_time: "12:45",
+        close_time: "15:55"
+    });
 });
