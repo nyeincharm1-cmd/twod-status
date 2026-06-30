35eb51a77b7e01c4d7d2775407cde93d29d9216f
nyeincharm1-cmd <nyeincharm1@gmail.com>
2026-07-01 00:18:39 +0630

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,39 @@ const express = require("express");
 const app = express();
 
 app.get("/status", (req, res) => {
+
+    const now = new Date();
+
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+
+    const current = hour * 60 + minute;
+
+    // 7:00 PM
+    const morningOpen = 19 * 60;
+
+    // 11:55 PM
+    const morningClose = 23 * 60 + 55;
+
+    // 12:45 PM
+    const eveningOpen = 12 * 60 + 45;
+
+    // 3:55 PM
+    const eveningClose = 15 * 60 + 55;
+
+    let status = "CLOSE";
+
+    if (
+        (current >= morningOpen && current <= morningClose) ||
+        (current >= eveningOpen && current <= eveningClose)
+    ) {
+        status = "OPEN";
+    }
+
     res.json({
+
+        status: status,
+
         morning_open: "07:00 PM",
         morning_close: "11:55 PM",
         morning_result: "12:01 PM",
@@ -10,11 +42,9 @@ app.get("/status", (req, res) => {
         evening_open: "12:45 PM",
         evening_close: "03:55 PM",
         evening_result: "04:30 PM"
+
     });
+
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-});
+app.listen(process.env.PORT || 3000);
