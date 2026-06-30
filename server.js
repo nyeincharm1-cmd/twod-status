973b44fc34b8cc6e8787037521191ed33df809e2
nyeincharm1-cmd <nyeincharm1@gmail.com>
2026-07-01 00:04:01 +0630

    server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const express = require("express");
+const app = express();
+
 app.get("/status", (req, res) => {
     res.json({
-
         morning_open: "07:00 PM",
         morning_close: "11:55 PM",
         morning_result: "12:01 PM",
@@ -8,6 +10,11 @@ app.get("/status", (req, res) => {
         evening_open: "12:45 PM",
         evening_close: "03:55 PM",
         evening_result: "04:30 PM"
-
     });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
