d765fbb3ee0f8bb373695854405592c5f3840bd3
nyeincharm1-cmd <nyeincharm1@gmail.com>
2026-06-30 23:55:45 +0630

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 app.get("/status", (req, res) => {
     res.json({
-        status: "CLOSE",
-        open_time: "12:45",
-        close_time: "15:55"
+
+        morning_open: "07:00 PM",
+        morning_close: "11:55 PM",
+        morning_result: "12:01 PM",
+
+        evening_open: "12:45 PM",
+        evening_close: "03:55 PM",
+        evening_result: "04:30 PM"
+
     });
 });
